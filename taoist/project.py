833e7496c44671ac7375fda9e91ccb150740668b
popgendad <popgendad@gmail.com>
2022-01-18 09:38:02 -0800

    Added read_project_list function
    Closes #14

--- taoist/project.py
+++ taoist/project.py
@@ -1,41 +1,36 @@
 """project.py"""
-import argparse
-from taoist.read_config import read_config
+from argparse import ArgumentParser
+from taoist.read_project_dict import read_project_dict
 from tabulate import tabulate
 from todoist_api_python.api import TodoistAPI
 
-def run_project(args: argparse.ArgumentParser) -> None:
+def run_project(args: ArgumentParser) -> None:
     """
     Run project function
     """
 
-    # Read taoist user configuration
-    config = read_config()
-
-    # Initialize Todoist API
-    api = TodoistAPI(config['Default']['token'])
+    # Read config and project list
+    config, project_dict = read_project_dict()
 
     # Process subcommand
     if args.subcommand == "list":
         table_header = ["id", "name"]
         project_list = [table_header,]
-        try:
-            projects = api.get_projects()
-        except Exception as error:
-            print(error)
-        for project in projects:
-            row = [project.id, project.name]
+        for key, project in project_dict.items():
+            row = [key, project.name]
             project_list.append(row)
-        
-        # Print project list table
         print(tabulate(project_list, headers="firstrow"))
     elif args.subcommand == "create":
+        api = TodoistAPI(config['Default']['token'])
         try:
             project = api.add_project(name=args.project_name)
         except Exception as error:
             print(error)
     elif args.subcommand == "delete":
+        api = TodoistAPI(config['Default']['token'])
         try:
             is_success = api.delete_project(project_id=args.project_id)
+            if is_success:
+                print(f"Deleted project {project_dict[args.project_id].name}")
         except Exception as error:
             print(error)        
